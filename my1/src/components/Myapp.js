012e39b1f8c3fc27160b6631ffb33106b59e7358
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2022-01-05 23:26:37 +0500

    takrorlash1

---
+++ my1/src/components/Myapp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Myapp extends Component {
+    render() {
+        return (
+            <div>
+                  <h1>Hi Node Js</h1>
+            </div>
+        )
+    }
+}
--- my1/src/index.js
+++
@@ -1,7 +0,0 @@
-import react from "react";
-
-import ReactDOM from "react-dom";
-
-const text = <h1>Node Js Backend Developer</h1>
-
-ReactDOM.render(text, document.getElementById("root"));
